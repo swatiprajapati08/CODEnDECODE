616b9667396cb7431bfd127ff8935e28017c716f
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-04-01 18:26:11 +0530

    Add files via upload
    About the contest
    This contest is part of DSA Learning Series
    The topic of the contest: Complexity Analysis + Basics Warm-Up
    Pre-requisite of the contest: None

---
+++ CodeChef/DSA Learning Course/Complexity Analysis + Basics Warm Up/TEST.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner =new Scanner(System.in);
+		try {
+		    int num;
+		while((num = scanner.nextInt())!=42)
+		{ 
+		    System.out.println(num);
+		}
+	//	System.exit(1);
+		} catch(Exception e) {
+		} finally {
+		}
+	}
+}
