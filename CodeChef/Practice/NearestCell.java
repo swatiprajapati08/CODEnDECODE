b36d1221493d5cc371334862d52f8ff0444df375
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-04-02 19:34:19 +0530

    Update NearestCell.java
    Distance of nearest cell having 1 in a binary matrix

--- CodeChef/Practice/NearestCell.java
+++ CodeChef/Practice/NearestCell.java
@@ -1,3 +1,23 @@
+/*Distance of nearest cell having 1 in a binary matrix
+Given a binary matrix of N x M, containing at least a value 1. The task is to find the distance of nearest 1 in the matrix for each cell. The distance is calculated as |i1 – i2| + |j1 – j2|, where i1, j1 are the row number and column number of the current cell and i2, j2 are the row number and column number of the nearest cell having value 1.
+
+Examples:
+
+Input : N = 3, M = 4
+        mat[][] = { 
+                    0, 0, 0, 1,
+                    0, 0, 1, 1,
+                    0, 1, 1, 0 
+                  }
+Output : 3 2 1 0
+         2 1 0 0
+         1 0 0 1
+
+For cell at (0, 0), nearest 1 is at (0, 3),
+so distance = (0 - 0) + (3 - 0) = 3.
+Similarly all the distance can be calculated.*/
+
+
 import java.util.*;
 import java.io.*;
 
