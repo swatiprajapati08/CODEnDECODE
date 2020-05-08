64e6f4b366d258cda7e98e25a533ad6e3e7e824a
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-05-08 06:38:42 +0530

    ATCODER
    PRACTICING EDUCATIONAL DP CONTEST FROM ATCODER

---
+++ %%DP%%/ATCODER/FROG-1.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+class Main {
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int arr[]=new int[n];
+	    for(int i=0;i<n;i++)
+	    arr[i]=sc.nextInt();
+	    int dp[]=new int[100];
+	    System.out.println(frog(arr,arr.length));
+	}
+static int frog(int arr[],int n)
+{
+    int dp[]=new int[n];
+    for(int i=1;i<n;i++)
+    {
+        int option1=Math.abs(arr[i]-arr[i-1])+dp[i-1];
+        int option2=(i==1)?Integer.MAX_VALUE:Math.abs(arr[i]-arr[i-2])+dp[i-2];
+        dp[i]=Math.min(option1,option2);
+    }
+    return dp[n-1];
+}
+}
